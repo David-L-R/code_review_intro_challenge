122066258fcdba20728cd166a386471c491cc9af
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-09-23 15:51:56 +0200

    created exe 3

--- exercises/exercise_3.js
+++ exercises/exercise_3.js
@@ -1,1 +1,12 @@
+const characters = require("../data/characters.json");
 
+// return only characters that their height is between 180 and 200
+
+const charactersWithMediumHeight = () => {
+  // YOUR CODE HERE
+  // YOU CAN CHANGE ANY CODE INSIDE THE FUNCTION
+  const MIN_HEIGHT = 0;
+  const MAX_HEIGHT = 300;
+};
+
+module.exports = charactersWithMediumHeight;
