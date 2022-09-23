d1676b572383a737dd7b91132ceaf4eb4b00c4bb
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-09-23 15:58:52 +0200

    created exe 5

--- exercises/exercise_5.js
+++ exercises/exercise_5.js
@@ -1,1 +1,16 @@
+const characters = require("../data/characters.json");
 
+// return all characters, but keep only the following fields:
+/*
+ * name
+ * height
+ * mass
+ * gender
+ * birth_year
+ */
+
+const charactersFormatted = () => {
+  //  YOUR CODE HERE
+};
+
+module.exports = charactersFormatted;
