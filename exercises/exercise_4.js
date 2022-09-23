1b8cd3bbee3c4e3e41993e23153971ab42302208
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-09-23 15:58:42 +0200

    created exe 4

--- exercises/exercise_4.js
+++ exercises/exercise_4.js
@@ -1,1 +1,16 @@
+const characters = require("../data/characters.json");
 
+// return only characters with the following properties:
+// fair skin color
+// blue eye color
+// female
+
+const charactersFemaleBlueEyesFairSkin = () => {
+  // YOUR CODE HERE
+  // YOU CAN CHANGE ANYTHING INSIDE THE FUNCTION
+  const SKIN = "";
+  const EYES = "";
+  const SEX = "";
+};
+
+module.exports = charactersFemaleBlueEyesFairSkin;
