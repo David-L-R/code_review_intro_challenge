6fa0c4157ce7265763c67815535a6a3c76507f99
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-09-23 15:51:47 +0200

    created exe 2

--- exercises/exercise_2.js
+++ exercises/exercise_2.js
@@ -1,1 +1,8 @@
+const characters = require("../data/characters.json");
 
+// TODO: return only characters with known birth year
+const charactersWithKnownBirthDate = () => {
+  // CODE HERE
+};
+
+module.exports = charactersWithKnownBirthDate;
