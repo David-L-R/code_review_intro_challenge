9c724512309409e164559cecad8fd9536b8cf5e8
David Rajcher <davidrajcher@Davids-MacBook-Pro-2.local>
2022-09-27 20:42:29 +0200

    refactor[exercise_1]: remove return

--- exercises/exercise_1.js
+++ exercises/exercise_1.js
@@ -2,8 +2,6 @@ const characters = require("../data/characters.json");
 
 /* TODO: return one character with an "unknown" birth year  */
 
-const findCharacterUnknownBirthDate = () => {
-  return characters.find((c) => c.birth_year === "unknown");
-};
-
+const findCharacterUnknownBirthDate = () =>
+  characters.find((c) => c.birth_year === "unknown");
 module.exports = findCharacterUnknownBirthDate;
