ef76304c13a0e1578d30b56cf1000cfe976c3fdc
michalbendavid <michalbendavid770@gmail.com>
2022-09-27 21:39:48 +0300

    still has a bug on 1 exe

--- exercises/exercise_1.js
+++ exercises/exercise_1.js
@@ -3,7 +3,9 @@ const characters = require("../data/characters.json");
 /* TODO: return one character with an "unknown" birth year  */
 
 const findCharacterUnknownBirthDate = () => {
-  return characters.birthYear==="unknown";
+   const filteredCharacters = characters.filter(characters.birthYear==="unknown");
+   return filteredCharacters[0];
  };
 
+// console.log(findCharacterUnknownBirthDate());
 module.exports = findCharacterUnknownBirthDate;
