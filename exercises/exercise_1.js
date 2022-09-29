8795d1eb1fabce56bb7962a0a27555f926b779df
michalbendavid <michalbendavid770@gmail.com>
2022-09-29 15:15:05 +0300

    exe 1

--- exercises/exercise_1.js
+++ exercises/exercise_1.js
@@ -1,11 +1,14 @@
 const characters = require("../data/characters.json");
 
-/* TODO: return one character with an "unknown" birth year  */
-
+// /* TODO: return one character with an "unknown" birth year  */
+// for(i=0; i<characters.length; i++){
+//   console.log(characters[i].birth_year);
+// }
 const findCharacterUnknownBirthDate = () => {
-   return characters.find(characters.birthYear==="unknown");
- 
- };
+ return characters.find((charecter) => charecter.birth_year  === "unknown");
+}
+// const findCharacterUnknownBirthDate = () => {
+//    return characters.find(characters.birth_year === "unknown");
+// }
 
-// console.log(findCharacterUnknownBirthDate());
 module.exports = findCharacterUnknownBirthDate;
