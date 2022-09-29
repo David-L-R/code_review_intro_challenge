f24671125abbbae9fd07c9fcfc45e70c03755a53
michalbendavid <michalbendavid770@gmail.com>
2022-09-29 15:50:45 +0300

    exe 4

--- exercises/exercise_4.js
+++ exercises/exercise_4.js
@@ -8,9 +8,12 @@ const characters = require("../data/characters.json");
 const charactersFemaleBlueEyesFairSkin = () => {
   // YOUR CODE HERE
   // YOU CAN CHANGE ANYTHING INSIDE THE FUNCTION
-  const SKIN = "";
-  const EYES = "";
-  const SEX = "";
+  const SKIN = "fair";
+  const EYES = "blue";
+  const SEX = "female";
+  return characters.filter(character => 
+    (character.skin_color === SKIN) && (character.eye_color === EYES) && (character.gender === SEX));
+   
 };
 
 module.exports = charactersFemaleBlueEyesFairSkin;
